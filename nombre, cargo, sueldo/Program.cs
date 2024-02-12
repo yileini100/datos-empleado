84372f72af8d0c42dbd9fd2d8f3aec19576690b7
yoga 11e <yoga 11e@DESKTOP-OOOUNS6>
2024-02-12 07:57:37 -0400

    Agregar archivos de proyecto.

---
+++ nombre, cargo, sueldo/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("datos del empleado");
+
+// Solicitar al usuario que ingrese los datos del empleado
+Console.WriteLine("Ingrese el nombre del empleado:");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese el cargo del empleado:");
+string cargo = Console.ReadLine();
+
+Console.WriteLine("Ingrese el sueldo del empleado:");
+double sueldo = Convert.ToDouble(Console.ReadLine());
+
+// Llamar al método para mostrar los resultados
+   MostrarInformacionEmpleado(nombre, cargo, sueldo);
+    
+
+    static void MostrarInformacionEmpleado(string nombre, string cargo, double sueldo)
+{
+    // Mostrar los resultados en pantalla
+    Console.WriteLine("Nombre del empleado: " + nombre);
+    Console.WriteLine("Cargo del empleado: " + cargo);
+    Console.WriteLine("Sueldo del empleado: " + sueldo);
+}
